fce20929731c5449519f96d836dcb9788f257a2d
danilbalistero <danilbalistero@gmail.com>
2024-09-09 14:07:27 -0300

    Atualizando

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const rotas = require('./routes');
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
-app.use('/api', rotas); 
+app.use('/api', rotas);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
